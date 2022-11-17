ddf9aeccf3bfa35ace7d0d3882a406b4a07298ad
DESKTOP-T987IHU\TY <3286415553@>
2022-11-17 11:08:22 +0800

    代码生成器：人人代码生成器 mysql5 tx-mybatis

--- start_demo_02/src/main/java/com/lp/start_demo_02/StringDemo.java
+++ start_demo_02/src/main/java/com/lp/start_demo_02/StringDemo.java
@@ -12,12 +12,13 @@ public class StringDemo {
 
     public static void main(String[] args) {
         String primaryKey = "2122D83C";
-
+        String str1 = "21044c77";
+        demo(str1);
     }
 
 
-    private static void demo() {
-        String str1 = "hello world";
-
+    private static void demo(String str) {
+        System.out.println(str.substring(0,4));
+        System.out.println(str.substring(4));
     }
 }
