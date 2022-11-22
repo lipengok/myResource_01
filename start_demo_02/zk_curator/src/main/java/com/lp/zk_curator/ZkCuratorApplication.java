aaf5b3fc63740f46adc1452ac86eafcee7efec70
DESKTOP-T987IHU\TY <3286415553@>
2022-11-22 15:32:11 +0800

    zookeeper管理工具curator的demo

---
+++ start_demo_02/zk_curator/src/main/java/com/lp/zk_curator/ZkCuratorApplication.java
@@ -0,0 +1,13 @@
+package com.lp.zk_curator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkCuratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkCuratorApplication.class, args);
+    }
+
+}
